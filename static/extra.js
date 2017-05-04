015d533f55a14b512fb5624b1d41549d3be309e0
7217238 <ff>
2017-05-04 17:55:59 +0200

    implementation with jQuery

--- static/extra.js
+++ static/extra.js
@@ -1,6 +1,41 @@
+$(document).ready(function(){
+    $('#inputdescription').focus(function(){
+        $('#submitbutton').removeAttr('disabled') ;
+    }) ;
+
+    $('.task-copy-icon').click(function(){
+        var text = $(this).parents('li.taskitem').text() ;
+        $('#inputdescription').val(text) ;
+    }) ;
+}) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created by 7217238 on 27/04/2017.
- */
 function myfocus() {
     document.getElementById("submitbutton").removeAttribute("disabled") ;
 }
@@ -10,3 +45,4 @@ function copydown(element) {
     document.getElementById("inputdescription").value=text ;
 
 }
+**/
